70579508b8cec8a4e09d44ac1d2a411b414b1350
zhavei <zhavei.vei@gmail.com>
2022-11-27 21:19:32 +0700

    all done tinggal cara deletenya doang

--- app/src/main/java/com/syafei/simplebottomsheet/repository/TodoApplication.kt
+++ app/src/main/java/com/syafei/simplebottomsheet/repository/TodoApplication.kt
@@ -2,8 +2,14 @@ package com.syafei.simplebottomsheet.repository
 
 import android.app.Application
 import com.syafei.simplebottomsheet.data.TaskItemDataBase
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.SupervisorJob
 
 class TodoApplication : Application() {
+
+    //val applicationScope = CoroutineScope(SupervisorJob())
+
+    //benefit this class is for an instance of database and repository then using it in MainActivity
     private val database by lazy {
         TaskItemDataBase.getDatabase(this)
     }
